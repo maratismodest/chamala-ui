79a2aa2543ff0cb3444e28d340d2e95dd6a31a33
Marat Faizerakhmanov <maratismodest@gmail.com>
2024-05-31 15:46:12 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import MyButton from "./MyButton/MyButton";
 import MyInput from "./MyInput/MyInput";
 import MyText from "./MyText/MyText";
+import MySwitcher from "./MySwitcher/MySwitcher";
 
-export { MyButton, MyInput, MyText };
+export { MyButton, MyInput, MyText,MySwitcher };
