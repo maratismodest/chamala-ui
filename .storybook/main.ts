b7e6b98285bc8af593c2c285a8690af6e13e4723
Marat Faizerakhmanov <maratismodest@gmail.com>
2025-12-25 19:20:37 +0200

    Revert "commit"
    This reverts commit 5aa499894aa87cfc276affa88a197c75d4df4f7d.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,8 +5,9 @@ const config: StorybookConfig = {
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-links",
+    "@storybook/addon-essentials",
     "@chromatic-com/storybook",
-    "@storybook/addon-docs"
+    "@storybook/addon-interactions",
   ],
   framework: {
     name: "@storybook/react-vite",
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import {Input} from "./Input/Input";
 import {Button} from "./Button/Button";
 import {Switcher} from "./Switcher/Switcher";
-import {Textarea} from "./Textarea/Textarea";
 
-export {Button, Input, Switcher, Textarea};
+export {Button, Input, Switcher};
